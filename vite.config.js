128fb40f074ea596d92880dc92736673f7332e75
Riad <kazuha3242@gmail.com>
2025-09-20 02:14:16 +0600

    Surely Last try on github deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+const repoName = 'GlobePeek'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: '/',
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
 })
