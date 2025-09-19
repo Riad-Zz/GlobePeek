535a47ca1aeca3513ec15ec5cedbf245eac2686b
Riad <kazuha3242@gmail.com>
2025-09-20 00:29:40 +0600

    Visited Country Details Modal Added

--- src/components/VisitedCountry/VisitedCoutry.jsx
+++ src/components/VisitedCountry/VisitedCoutry.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const VisitedCoutry = ({country}) => {
+const VisitedCoutry = ({ country }) => {
     // console.log(country) ;
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <div>
-            <button className='btn rounded-xl'>{country.name.common}</button>
+            <button onClick={() => setIsOpen(true)} className='btn rounded-xl'>{country.name.common}</button>
+            {
+                isOpen && (
+                    <div className="modal modal-open">
+                        <div className="modal-box">
+
+                            <div className=' border-1 px-2 py-2 rounded-xl'>
+                                <div className='flex justify-center '>
+                                    <img src={country.flags.flags.png} alt="" className='rounded-xl' />
+                                </div>
+                                <div className='text-center py-2'>
+                                    <p className='text-2xl font-bold '>{country.name.common}</p>
+                                    <p className='pt-2'>Polulation : {country.population.population}</p>
+                                    <p className='pt-2'>Region : {country.region.region}</p>
+                                    {/* <button onClick={hadleVisitedButton} className={`btn btn-wide ${status ? ` bg-green-500` : `bg-red-400`} my-2 rounded-2xl`}>{status ? "Visited" : "Not Visited"}</button> */}
+                                </div>
+                            </div>
+
+                            <div className="modal-action justify-start">
+                                <button className="btn" onClick={() => setIsOpen(false)}>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
